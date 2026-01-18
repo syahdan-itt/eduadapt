ea2d81da391dac8e05bb9443874c004f2a616618
Diovalda22 <ferrenxx@gmail.com>
2026-01-18 22:50:03 +0700

    fix: api route alias

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,15 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\Admin\MaterialController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\MaterialController;
-use App\Http\Controllers\Api\Admin\SubjectController;
-use App\Http\Controllers\Users\SubjectController;
+use App\Http\Controllers\Api\AuthController;;
+use App\Http\Controllers\Users\SubjectController as UserSubjectController;
 use App\Http\Controllers\Users\QuizController;
-
-Route::post('/auth/register', [AuthController::class, 'register'])->middleware('throttle:5,1');
-Route::post('/auth/login', [AuthController::class, 'login'])->middleware('throttle:10,1');
+use App\Http\Controllers\Api\Admin\SubjectController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
@@ -17,25 +14,16 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
 
-    Route::middleware('role:1')->prefix('admin')->group(function () {
-
-        Route::prefix('subject')->group(function () {
-            Route::get('/', [SubjectController::class, 'index']);
-            Route::get('/{id}', [SubjectController::class, 'show']);
-        });
-
-        // Route::get('/materials', [MaterialController::class, 'index']);
-        // Route::get('/materials/{id}', [MaterialController::class, 'show']);
-        // Route::post('/materials', [MaterialController::class, 'store']);
-        // Route::put('/materials/{id}', [MaterialController::class, 'update']);
-        // Route::delete('/materials/{id}', [MaterialController::class, 'destroy']);
-
-        // Generate AI Content
-        Route::post('/materials/{id}/generate-adaptive', [MaterialController::class, 'generateAdaptive']);
+    Route::middleware('role:1')->group(function () {
+        Route::get('/materials', [MaterialController::class, 'index']);
+        Route::get('/materials/{id}', [MaterialController::class, 'show']);
+        Route::post('/materials', [MaterialController::class, 'store']);
+        Route::put('/materials/{id}', [MaterialController::class, 'update']);
+        Route::delete('/materials/{id}', [MaterialController::class, 'destroy']);
     });
 
     Route::middleware('role:2')->group(function () {
-        Route::get('/materials', [MaterialController::class, 'index']);
+        Route::get('/materials', [UserSubjectController::class, 'index']);
     });
 
     Route::middleware('role:2')->prefix('user')->group(function () {
